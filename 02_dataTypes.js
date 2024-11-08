51349ea2aa17f29e7b3258af433207b96439caa7
Mandar01-ops <mandarzadpe123@gmail.com>
2024-11-08 09:14:30 +0000

    datattypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using node.js not browser
+
+console.log(3+3)
+console.log("Mandar")
+
+let name="mandar"
+let age=21
+let isLoggedIn=true
+let state;
+
+// number => 2 to power of 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
